289a2f2ba06e9ef484dba3e21588668fb7ed5d1f
Sabrina Lydia Simanjuntak <83162422+sabrinalydias@users.noreply.github.com>
2022-06-14 12:33:15 +0700

    Dashboard fix

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+
+namespace Dash
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Series 1",
+                    Values = new ChartValues<double> { 4, 6, 5, 2 }
+                },
+                new LineSeries
+                {
+                    Title = "Series 2",
+                    Values = new ChartValues<double> { 6, 7, 3, },
+                    PointGeometry = null
+                },
+                new LineSeries
+                {
+                    Title = "Series 3",
+                    Values = new ChartValues<double> { 4,2,7 },
+                }
+            };
+
+            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+            YFormatter = value => value.ToString("C");
+
+            //modifying the series collection will animate and update the chart
+            SeriesCollection.Add(new LineSeries
+            {
+                Title = "Series 4",
+                Values = new ChartValues<double> { 5, 3, 2, 4 },
+                LineSmoothness = 1, //0: straight lines, 1: really smooth lines
+            });
+
+            //modifying any series values will also animate and update the chart
+            SeriesCollection[3].Values.Add(5d);
+
+            //DataContext = this;
+
+
+            PSeriesCollection = new SeriesCollection
+            {
+                new StackedRowSeries
+                {
+                    Values = new ChartValues<double> {4, 5, 6, 8},
+                    StackMode = StackMode.Percentage,
+                    DataLabels = true,
+                    LabelPoint = p => p.X.ToString()
+                },
+                new StackedRowSeries
+                {
+                    Values = new ChartValues<double> {2, 5, 6, 7},
+                    StackMode = StackMode.Percentage,
+                    DataLabels = true,
+                    LabelPoint = p => p.X.ToString()
+                }
+            };
+
+            //adding series updates and animates the chart
+            PSeriesCollection.Add(new StackedRowSeries
+            {
+                Values = new ChartValues<double> { 6, 2, 7 },
+                StackMode = StackMode.Percentage,
+                DataLabels = true,
+                LabelPoint = p => p.X.ToString()
+            });
+
+            //adding values also updates and animates
+            PSeriesCollection[2].Values.Add(4d);
+
+            PLabels = new[] { "Chrome", "Mozilla", "Opera", "IE" };
+            PFormatter = val => val.ToString("P");
+
+            //DataContext = this;
+
+
+            DSeriesCollection = new SeriesCollection
+            {
+                new PieSeries
+                {
+                    Title = "Chrome",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(8) },
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "Mozilla",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(6) },
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "Opera",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(10) },
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "Explorer",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(4) },
+                    DataLabels = true
+                }
+            };
+
+
+            DataContext = this;
+
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+
+        public SeriesCollection PSeriesCollection { get; set; }
+        public string[] PLabels { get; set; }
+        public Func<double, string> PFormatter { get; set; }
+
+        public SeriesCollection DSeriesCollection { get; set; }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+    }
+
+
+
+}
